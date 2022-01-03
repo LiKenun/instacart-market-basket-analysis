f855c22f9fa91097558a8cd69dc566951c0bba15
Kevin Li <lku@kenun.li>
2022-01-03 13:08:02 -0500

    Initial unit tests for LemmatizerService and ProductLookupService

--- test/test_services.py
+++ test/test_services.py
@@ -1,15 +1,78 @@
+from toolz import thread_last as thread
 import unittest
-from repositories import *
+from models import *
 from services import *
 
 
 class TestServices(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.lemmatizer_service = LemmatizerService()
-
     def test_LemmatizerService(self):
-        pass
+        with self.subTest('tokenize'):
+            with self.subTest('“apples, bananas, and carrots” tokenizes to {“apples”, “bananas”, “and”, “carrots”}'):
+                self.assertSequenceEqual(tuple(LemmatizerService.tokenize('apples, bananas, and carrots')),
+                                         ('apples', 'bananas', 'and', 'carrots'))
+        with self.subTest('lemmatize'):
+            lemmatizer_service = LemmatizerService()
+            with self.subTest('“apples, bananas, and carrots” produces lemmas {“apple”, “banana”, “carrot”}'):
+                self.assertSequenceEqual(tuple(lemmatizer_service.lemmatize('apples, bananas, and carrots')),
+                                         (('apple', 'apples'), ('banana', 'bananas'), ('carrot', 'carrots')))
+
+    def test_ProductLookupService(self):
+        # The products are sorted in descending order (by identifier).
+        # These are taken from https://www.javatpoint.com/apriori-algorithm-in-machine-learning
+        products = \
+            ('Chicken', 'Light Cream', 'Escalope', 'Mushroom Cream Sauce', 'Pasta', 'Fromage Blanc', 'Honey',
+             'Ground Beef', 'Herb & Pepper', 'Tomato Sauce', 'Olive Oil', 'Whole Wheat Pasta', 'Shrimp')
+        rules = \
+            (Rule((),   (0,),   Measure(1.0,                0.004533333333333334)),
+             Rule((),   (1,),   Measure(1.0,                0.007733333333333334)),
+             Rule((),   (2,),   Measure(1.0,                0.0116)),
+             Rule((),   (3,),   Measure(1.0,                0.005733333333333333)),
+             Rule((),   (4,),   Measure(1.0,                0.005866666666666667)),
+             Rule((),   (5,),   Measure(1.0,                0.0033333333333333335)),
+             Rule((),   (6,),   Measure(1.0,                0.0033333333333333335)),
+             Rule((),   (7,),   Measure(1.0,                0.021333333333333333)),
+             Rule((),   (8,),   Measure(1.0,                0.016)),
+             Rule((),   (9,),   Measure(1.0,                0.005333333333333333)),
+             Rule((),   (10,),  Measure(1.0,                0.0112)),
+             Rule((),   (11,),  Measure(1.0,                0.008)),
+             Rule((),   (12,),  Measure(1.0,                0.005066666666666666)),
+             Rule((0,),  (1,),  Measure(4.843304843304844,  0.004533333333333334)),
+             Rule((2,),  (3,),  Measure(3.7903273197390845, 0.005733333333333333)),
+             Rule((2,),  (4,),  Measure(4.700185158809287,  0.005866666666666667)),
+             Rule((4,),  (12,), Measure(4.514493901473151,  0.005066666666666666)),
+             Rule((5,),  (6,),  Measure(5.178127589063795,  0.0033333333333333335)),
+             Rule((7,),  (8,),  Measure(3.2915549671393096, 0.016)),
+             Rule((9,),  (7,),  Measure(3.840147461662528,  0.005333333333333333)),
+             Rule((10,), (1,),  Measure(3.120611639881417,  0.0032)),
+             Rule((10,), (11,), Measure(4.130221288078346,  0.008)))
+        product_lookup_service = \
+            ProductLookupService(type('', (), {'products_data_file': '', 'get_all_products': (lambda _: products)})(),
+                                 type('', (), {'rules_data_file': '', 'get_all_rules': (lambda _: rules)})(),
+                                 LemmatizerService())
+        with self.subTest('Empty basket and null query returns suggestions with no antecedent items'):
+            self.assertTrue(thread(product_lookup_service.get_suggestions(),
+                                   (map, lambda suggestion: suggestion.antecedent_items == ()),
+                                   all))
+        with self.subTest('Olive oil in basket suggests whole wheat pasta, light cream, …'):
+            self.assertSequenceEqual(thread({products.index('Olive Oil')},
+                                            product_lookup_service.get_suggestions,
+                                            (map, lambda suggestion: suggestion.product.name),
+                                            tuple),
+                                     ('Whole Wheat Pasta', 'Light Cream', 'Ground Beef', 'Herb & Pepper', 'Escalope',
+                                      'Pasta', 'Mushroom Cream Sauce', 'Tomato Sauce', 'Shrimp', 'Chicken'))
+        with self.subTest('Query for “cre” suggests both light cream and mushroom cream sauce'):
+            self.assertSequenceEqual(thread(product_lookup_service.get_suggestions(query='cream'),
+                                            (map, lambda suggestion: suggestion.product.name),
+                                            tuple),
+                                     ('Light Cream', 'Mushroom Cream Sauce'))
+        with self.subTest('Query for “cre” with escalope in basket suggests mushroom cream sauce and light cream'):
+            self.assertSequenceEqual(thread(product_lookup_service.get_suggestions({products.index('Escalope')}, 'cre'),
+                                            (map, lambda suggestion: suggestion.product.name),
+                                            tuple),
+                                     ('Mushroom Cream Sauce', 'Light Cream'))
+        with self.subTest('Query for text that does not exist in any product names results in zero suggestions'):
+            self.assertSequenceEqual(product_lookup_service.get_suggestions(query='gmijul[kfakl,kim49iklgijldvs48n5wi'),
+                                     ())
 
 
 if __name__ == '__main__':
